79ffd0064a294790ba40a3fb7115db7af78c47b9
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2025-01-13 13:13:04 +0100

    Database Game update. Inserts database created

--- escaperoomV2.sql
+++ escaperoomV2.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2025-01-10 18:51
+-- Generated: 2025-01-13 12:56
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -21,31 +21,23 @@ ALTER TABLE `escaperoomdb`.`Tips`
 CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`ObjectDeco` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-DROP COLUMN `ObjectDeco_deleted`,
-ADD COLUMN `ObjectDeco_deleted` TINYINT(4) NULL DEFAULT NULL AFTER `ObjectDeco_price`;
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`Game` 
 CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-DROP COLUMN `Game_deleted`,
-ADD COLUMN `Game_deleted` TINYINT(4) NULL DEFAULT NULL AFTER `Game_escapeRoomId`;
+ADD COLUMN `Game_finished` TINYINT(4) NULL DEFAULT NULL AFTER `Game_escapeRoomId`;
 
 ALTER TABLE `escaperoomdb`.`gameHasPlayer` 
 CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`Player` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-DROP COLUMN `Player_deleted`,
-ADD COLUMN `Player_deleted` TINYINT(4) NULL DEFAULT NULL AFTER `Player_consentNotif`;
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`Sale` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-DROP COLUMN `Sale_deleted`,
-ADD COLUMN `Sale_deleted` TINYINT(4) NULL DEFAULT NULL AFTER `Sale_gameId`;
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`Ticket` 
-CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
-ADD COLUMN `Ticket_text` VARCHAR(100) NULL DEFAULT NULL AFTER `Ticket_saleId`;
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
 ALTER TABLE `escaperoomdb`.`Certificate` 
 CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
