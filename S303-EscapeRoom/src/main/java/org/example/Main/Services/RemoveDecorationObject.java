52c037535ad519d9aaa32ec3080661479fa00e6a
pjab5 <137177443+pjab5@users.noreply.github.com>
2025-01-21 12:07:17 +0100

    Controller

---
+++ S303-EscapeRoom/src/main/java/org/example/Main/Services/RemoveDecorationObject.java
@@ -0,0 +1,22 @@
+package org.example.Main.Services;
+
+import org.example.Modules.Entities.RoomEntities.ObjectDeco;
+import org.example.Modules.Entities.RoomEntities.Room;
+import org.example.Repository.Common.DatabaseConnection;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class RemoveDecorationObject implements ServiceProcessor{
+    @Override
+    public void process(DatabaseConnection dbc, Scanner read) {
+        String name;
+        System.out.println("What room do you want to delete?");
+        name = read.next();
+        for(ObjectDeco o:dbc.getAllRooms()){
+            if(Objects.equals(name, o.getName())){
+
+            }
+        }
+    }
+}
