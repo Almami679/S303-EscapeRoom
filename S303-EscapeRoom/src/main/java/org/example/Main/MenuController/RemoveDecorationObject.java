69c4f0c10181702b9f6b009841a9068e34f15eb2
pjab5 <137177443+pjab5@users.noreply.github.com>
2025-01-22 11:48:33 +0100

    menu controller

--- S303-EscapeRoom/src/main/java/org/example/Main/MenuController/RemoveDecorationObject.java
+++ S303-EscapeRoom/src/main/java/org/example/Main/MenuController/RemoveDecorationObject.java
@@ -1,6 +1,8 @@
 package org.example.Main.MenuController;
 
 
+import org.example.Main.Services.EscapeRoomServices.ObjectDecoService;
+import org.example.Modules.Entities.RoomEntities.ObjectDeco;
 import org.example.Repository.Common.RepositoryImpl;
 
 import java.util.Scanner;
@@ -8,13 +10,10 @@ import java.util.Scanner;
 public class RemoveDecorationObject implements ServiceProcessor {
     @Override
     public void process(Scanner read, RepositoryImpl repository) {
-        String name;
-        System.out.println("What room do you want to delete?");
-        name = read.next();
-        /*for(ObjectDeco o:dbc.getAllRooms()){
-            if(Objects.equals(name, o.getName())){
-
-            }
-        }*/
+        int id;
+        System.out.println("What is the ID of the object you want to delete?");
+        id = read.nextInt();
+        ObjectDecoService ods = new ObjectDecoService(repository);
+        ods.deleteObjectDeco(id);
     }
 }
