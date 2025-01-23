ae04f02d85db7eaae03ec4d5fab552c1f10ac4d0
Almami679 <almami989@gmail.com>
2025-01-23 11:02:59 +0100

    Añadida clase UsersInput

--- S303-EscapeRoom/src/main/java/org/example/Main/Services/AddDecorationObject.java
+++ S303-EscapeRoom/src/main/java/org/example/Main/Services/AddDecorationObject.java
@@ -9,16 +9,16 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Scanner;
 
+import static org.example.Main.MenuController.UserInputs.askString;
+
 public class AddDecorationObject implements ServiceProcessor {
     @Override
     public void process(Scanner read, RepositoryImpl repository) {
         ObjectDeco obectDeco = null;
         String name, material, difficulty;
         double price;
-        System.out.println("What is the decoration object's name?");
-        name = read.next();
-        System.out.println("What is the object's material?");
-        material = read.next();
+        name = askString("What is the decoration object's name?",read);
+        material = askString("What is the object's material?",read);
         System.out.println("What is the object's price?");
         price = Double.parseDouble(read.next());
         System.out.println("What is the Room's difficulty?");
